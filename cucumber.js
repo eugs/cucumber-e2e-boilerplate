5c4fce31eea4ad13e1694c50e284b665d0539630
Eugen Stotik <eugen.stotik@wolterskluwer.com>
2022-04-08 15:59:51 +0400

    Package.json updated. Cucumber profile

--- cucumber.js
+++ cucumber.js
@@ -1,6 +1,7 @@
 const stepDefinitions = '--require step_definition/*.js';
 const jsonFormatter = '--format json:report/report.json';
+const allFeatures = 'feature/**/*.feature';
 
 module.exports = {
-    default: `${stepDefinitions} ${jsonFormatter}`
+    default: `${stepDefinitions} ${jsonFormatter} ${allFeatures}`
 }
